8c340fcf53e3e6721d42f676507ca059d4f28c7d
Dave Rintoul <drintoul@gmail.com>
2023-12-15 16:59:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,8 @@ st.write("""
 # Universal Orlando Waittimes
 """)
 
-st.write(df)
+#st.write(df)
 
-st.map(df, size=20)
+st.map(df, size=10)
+
+#st.map(data=None, latitude=28.358, longitude=81.59, color=None, size=None, zoom=None, use_container_width=True)

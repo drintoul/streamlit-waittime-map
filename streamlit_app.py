a855cc908750c9216071de4582517c5c4241732f
Dave Rintoul <drintoul@gmail.com>
2023-12-16 12:42:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ col1, col2 = st.columns(2)
 col1.dataframe(df[['time', f'{attraction}']], hide_index=True)
 col2.line_chart(chart_data)
 
-st.map(locations, size=8)
-st.dataframe(locations[locations['attraction'] == f'{attraction}'], hide_index=True)
-
-st.write(df[f'{attraction}'].describe().T)
+col1, col2 = st.columns(2)
+col1.map(locations, size=8)
+col2.dataframe(locations[locations['attraction'] == f'{attraction}'], hide_index=True)
+col2.write(df[f'{attraction}'].describe().T)

fe03afc7ea6a893ca9a23726f0bee7eeddfc8fb5
Dave Rintoul <drintoul@gmail.com>
2023-12-16 13:31:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,10 @@ with col2:
 
 col1, col2 = st.columns(2)
 with col1:
+  st.dataframe(locations[['attractions', 'color']])
   locations['color'] = '#000000'
   locations.loc[locations['attraction'] == f'{attraction}']['color'] = '#FF0000'
-  st.dataframe(locations)
+  st.dataframe(locations[['attractions', 'color']])
   #st.map(locations, size=8, color='color')
 with col2:
   st.write(df[f'{attraction}'].describe())

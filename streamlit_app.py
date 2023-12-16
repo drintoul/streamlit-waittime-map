3d8a8d7005a4a22f636da9e316f0eb508ce29387
Dave Rintoul <drintoul@gmail.com>
2023-12-16 09:25:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,13 @@ st.dataframe(locations[locations['attraction'] == attraction], hide_index=True)
 df = pd.read_csv('wait.csv')
 df = df.round(0)
 
-st.write(df[attraction].describe())
+col1, col2 = st.columns(2)
+col1.write(df[attraction].describe())
+col2.dataframe(df[['time', attraction]], hide_index=True)
 
 st.map(locations, size=8)
 
-st.dataframe(df[['time', attraction]], hide_index=True)
+
 
 #chart_data = df[attraction]
 #st.line_chart(chart_data)

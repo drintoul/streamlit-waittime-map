80c435756955208c8d7289aef0dabdebb2affdba
Dave Rintoul <drintoul@gmail.com>
2023-12-16 16:54:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,11 @@ locations = pd.read_excel('locations.xlsx')
 attractions = list(locations['attraction'].unique())
 attraction = st.selectbox('Attraction', attractions)
 
-#df = pd.read_csv('wait_jan.csv')
-#jan_data = df.round(0)
+#jan_data = pd.read_csv('wait_jan.csv').round(0)
 #jan_data = jul_data[['time', f'{attraction}']]
 
-df = pd.read_csv('wait_jul.csv')
-jul_data = df.round(0)
+jul_data = pd.read_csv('wait_jul.csv').round(0)
 jul_data = jul_data[['time', f'{attraction}']]
-
 
 col1, col2 = st.columns(2)
 with col1:
@@ -30,5 +27,3 @@ with col1:
   st.map(locations, size=8, color='#000000')
 with col2:
   st.write(jul_data[f'{attraction}'].describe())
-
-#col2.dataframe(locations[locations['attraction'] == f'{attraction}'], hide_index=True)

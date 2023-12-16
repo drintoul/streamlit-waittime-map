7212d1a9d9f11eb43a3225eb9719bd409357b9db
Dave Rintoul <drintoul@gmail.com>
2023-12-16 13:07:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ with col2:
 col1, col2 = st.columns(2)
 with col1:
   st.map(locations, size=8, color='color')
+  locations['color'] = 'black'
   locations.loc[f'{attraction}', 'color'] = 'red'
 with col2:
   st.write(df[f'{attraction}'].describe())

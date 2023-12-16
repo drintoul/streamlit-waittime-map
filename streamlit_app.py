3a9b8cf6359139f61d1c16f733da4b926b2fb291
Dave Rintoul <drintoul@gmail.com>
2023-12-16 13:01:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,16 @@ df = df.round(0)
 chart_data = df[['time', f'{attraction}']]
 
 col1, col2 = st.columns(2)
-col1.dataframe(df[['time', f'{attraction}']], hide_index=True)
-col2.line_chart(data = chart_data, x='time')
+with col1:
+  st.dataframe(df[['time', f'{attraction}']], hide_index=True)
+with col2:
+  st.line_chart(data = chart_data, x='time')
 
 col1, col2 = st.columns(2)
-col1.map(locations, size=8)
-#col1.point()
-col2.write(df[f'{attraction}'].describe())
+with col1:
+  st.map(locations, size=8)
+  #col1.point()
+with col2:
+  st.write(df[f'{attraction}'].describe())
+
 #col2.dataframe(locations[locations['attraction'] == f'{attraction}'], hide_index=True)

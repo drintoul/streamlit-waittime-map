a12f9717fe529b97d3093a077c1a3a8152f3c60b
Dave Rintoul <drintoul@gmail.com>
2023-12-16 17:15:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,12 +18,12 @@ jul_data = jul_data[['time', f'{attraction}']]
 
 col1, col2 = st.columns(2)
 with col1:
-  st.dataframe(jan_data[['time', f'{attraction}']], hide_index=True)
+  st.dataframe(jul_data[['time', f'{attraction}']], hide_index=True)
 with col2:
-  st.line_chart(data = jan_data, x='time')
+  st.line_chart(data = jul_data, x='time')
 
 col1, col2 = st.columns(2)
 with col1:
   st.map(locations, size=8, color='#000000')
 with col2:
-  st.write(jan_data[f'{attraction}'].describe())
+  st.write(jul_data[f'{attraction}'].describe())

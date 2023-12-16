5c243b166aa26b5f4cbeeb4c160d00f2f99750bb
Dave Rintoul <drintoul@gmail.com>
2023-12-16 09:17:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,7 @@ import pandas as pd
 
 st.title("""Universal Studios Wait Times""")
 
-st.subheader("""Averages from July 2021-2023
-Data from thrill-data.com""")
-
-st.info('Purpose of this project was to get experience using interactive streamlit controls, specifically with map and time widgets', icon='ℹ️')
+st.info('Averages from July 2021-2023. Data from thrill-data.com', icon='ℹ️')
 
 locations = pd.read_excel('locations.xlsx')
 
